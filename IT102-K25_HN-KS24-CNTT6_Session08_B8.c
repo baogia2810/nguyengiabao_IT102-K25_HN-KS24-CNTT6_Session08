60b09f255dfeb2a1cf0ffd48694f0352d660bbbc
baogia2810 <bankaijun2880@gmail.com>
2025-08-06 11:41:24 +0700

    upload bài 8

---
+++ IT102-K25_HN-KS24-CNTT6_Session08_B8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Nhap so hang: ");
+    scanf("%d", &rows);
+    printf("Nhap so cot: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+
+    printf("Nhap %d phan tu cho mang:\n", rows * cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Nhap phan tu [%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("Mang vua nhap la:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
